8627b3c4ab6d93569132329f8b673a21e81300ea
Mohammad Zafar Tamim <137557791+tamimuiuc@users.noreply.github.com>
2023-10-03 12:59:16 -0700

    Update search_eval.py

--- search_eval.py
+++ search_eval.py
@@ -11,14 +11,14 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25()
+    return metapy.index.OkapiBM25(k1=1.525, b=0.75, k3 = 3.235)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} config.toml".format(sys.argv[0]))
-        sys.exit(1)
+    #if len(sys.argv) != 2:
+        #print("Usage: {} config.toml".format(sys.argv[0]))
+        #sys.exit(1)
 
-    cfg = sys.argv[1]
+    cfg = 'config.toml' #sys.argv[1]
     print('Building or loading index...')
     idx = metapy.index.make_inverted_index(cfg)
     ranker = load_ranker(cfg)
